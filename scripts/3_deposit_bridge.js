22a8c395593761efd2df98d69669905222584331
sahil3vedi <sahiltrivediw@gmail.com>
2023-10-11 16:13:38 +0530

    initialised script for depositing to Bridge

---
+++ scripts/3_deposit_bridge.js
@@ -0,0 +1,47 @@
+const ethers = require('ethers');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+async function main(){
+    const privateKey = process.env.ROOT_KEY;
+    const providerURL = process.env.ROOT_RPC;
+    const childUser = process.env.CHILD_USER;
+    const rootToken = process.env.ROOT_ERC20;
+    const rootBridge = process.env.ROOT_BRIDGE
+
+    const provider = new ethers.JsonRpcProvider(providerURL);
+    const wallet = new ethers.Wallet(privateKey, provider);
+    const signer = wallet.connect(provider);
+
+    const destinationNetworkValue = 1234;  // Replace with actual value
+    const amountValue = ethers.parseUnits("1.0", 18);  // Replace "1.0" with the actual amount. Assumes token has 18 decimals.
+    const forceUpdateGlobalExitRootValue = true;  // or false, depending on your need
+    const permitDataValue = "0x0123";  // example byte values
+
+
+    const {abi} = require("../contracts/Bridge.json")
+    const bridgeContract = new ethers.Contract(rootBridge, abi, signer)
+   
+    const tx = await bridgeContract.bridgeAsset(
+        destinationNetworkValue,
+        childUser,
+        amountValue,
+        rootToken,
+        forceUpdateGlobalExitRootValue,
+        permitDataValue
+    );
+
+    console.log("Transaction hash:", tx.hash);
+
+    // Wait for the transaction to be mined
+    const receipt = await tx.wait();
+    console.log("Transaction was mined in block:", receipt.blockNumber);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
