cbc0fa074ae33fbd4271c467e469267782bcc270
sahil3vedi <sahiltrivediw@gmail.com>
2023-10-16 17:24:41 +0530

    added approve txn before bridgeAsset

--- scripts/3_deposit_bridge.js
+++ scripts/3_deposit_bridge.js
@@ -14,14 +14,28 @@ async function main(){
     const wallet = new ethers.Wallet(privateKey, provider);
     const signer = wallet.connect(provider);
 
-    const destinationNetworkValue = 1;  // Replace with actual value
     const amountValue = ethers.parseUnits("1.0", 18);  // Replace "1.0" with the actual amount. Assumes token has 18 decimals.
+
+    // Calling approve on Token
+
+    const token_abi = require("../contracts/MyToken.json").abi
+    const tokenContract = new ethers.Contract(rootToken, token_abi, signer)
+
+    const approve_tx = await tokenContract.approve(
+        rootBridge,
+        amountValue
+    )
+
+    console.log("Approve Transaction Hash:", approve_tx.hash)
+
+    // Calling bridgeAsset on Bridge Contract
+
+    const destinationNetworkValue = 1;  // Replace with actual value
     const forceUpdateGlobalExitRootValue = false;  // or false, depending on your need
     const permitDataValue = "0x0123";  // example byte values
 
-
-    const {abi} = require("../contracts/Bridge.json")
-    const bridgeContract = new ethers.Contract(rootBridge, abi, signer)
+    const bridge_abi = require("../contracts/Bridge.json").abi
+    const bridgeContract = new ethers.Contract(rootBridge, bridge_abi, signer)
    
     const tx = await bridgeContract.bridgeAsset(
         destinationNetworkValue,
